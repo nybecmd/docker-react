800ddf6037d9441156a225be4746c0fe56a30dc2
Cloud User <ec2-user@ip-172-31-8-142.us-west-2.compute.internal>
2019-12-18 15:16:00 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -16,8 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          <p>Learn React</p></a>
+          <p>I was changed</p>
+        
       </header>
     </div>
   );
